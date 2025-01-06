8f255b33b590fcbed9981f8b3b66845183c773ca
Sajiyaking <157352890+Sajiyaking@users.noreply.github.com>
2025-01-06 13:14:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,1 @@
-const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
-}
-module.exports = {
-SESSION_ID: process.env.SESSION_ID,
-};
+SESSION_ID process.env.SESSION_ID || "add you session id",
